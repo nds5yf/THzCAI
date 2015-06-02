b69e93bd7172c5ed65f300a0dbe4cf7f6f5a7668
nds5yf <nds5yf@virginia.edu>
2015-06-02 17:16:59 -0400

    Generation

--- Research/Python Scripts/SlideGeneration.py
+++ Research/Python Scripts/SlideGeneration.py
@@ -7,19 +7,52 @@ import math
 from Tkinter import *
 from collections import namedtuple
 
-myStruct = namedtuple("myStruct", "field1 field2")
 
-x = input("input the matrix dimension: ")
+x = input("Input the matrix dimension: ")
 
 master = Tk()
+wi = 0
+he = 0
+area = x * x
+size = math.pow(2, area)
+sqw = 600/x
+sqh = 600/x
 
-w = Canvas(master, width = 1200, height = 1200)
+
+bn = '{0:0' + str(area) + 'b}'
+
+top = 0
+li = []
+while top < size:
+    
+    mask = bn.format(top)
+    li.append(mask)
+    top = top + 1
+
+w = Canvas(master, width = 600, height = 600)
 w.pack()
-w.create_rectangle(0, 0, 1200, 1200, fill="white")
+w.create_rectangle(0, 0, 600, 600, fill="white")
 
-clist = list()
-
-for i in range(0, x*x):
-    w.create_rectangle(0,0,400,400,fill="black")
+#for i in range(0, int(size)):
+p = li[1]
+  
+color = "black"      
+for j in range (0, area):
+    if p[j] == "0":
+        color = "black"
+        print "black"
+    if p[j] == "1":
+        color = "white" 
+        print "white" 
+   
+    for t in range (0, x):  
+        if j % x == t:
+            wi = t
+        if j >= t*x:
+            he = t   
+    print wi 
+    print he 
+    w.create_rectangle(wi*sqw,he*sqh,(wi*sqw)+sqw,(he*sqh)+sqh,fill=color)
+  
 
 mainloop()
