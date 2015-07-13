79a425e772459aca835b9ac82e32e5f43584abbb
Michael Eller <mbe9a@virginia.edu>
2015-07-13 11:03:13 -0400

    inheritance

--- vecap/devices.py
+++ vecap/devices.py
@@ -3,11 +3,11 @@
 #9 July 2015
 
 import visa
-from skrf.vi import vna
+from skrf.vi.vna import ZVA40
 import numpy
 import time as time
 
-class ANA(object):
+class ZVA(ZVA40):
 	def __init__(self):
 		'''
 		A class to easily take and write data with the vna
@@ -16,13 +16,11 @@ class ANA(object):
 			vna object from skrf.vi (zva)
 
 		'''
-
+		ZVA40.__init__(self, address = 20)
 		#you will want the ZVA to be connected when you initialize
-		self.zva = vna.ZVA40(address=20)
-
 	def write_data(self,name):
 
-		self.zva.get_network().write_touchstone(name)
+		self.get_network().write_touchstone(name)
 
 class ESP(object):
 	def __init__(self):
@@ -37,11 +35,10 @@ class ESP(object):
 
 		#assumes only one resource
 		self.inst = rm.open_resource(rm.list_resources()[0])
-		self.inst.timeout = 25000
 		print(self.inst.query("*IDN?"))
 
 	def current_position(self):
-
+		
 		return float(self.inst.ask('1TP'))
 
 	def move(self, x):
@@ -49,7 +46,6 @@ class ESP(object):
 			return
 		else:
 			self.inst.write('1PA'+ numpy.str(x))
-			while float(self.inst.ask('1MD?')) < 1:
-				time.sleep(0.5)
-
+		while float(self.inst.ask('1TP')) - x != 0:
+			time.sleep(0.01)
 
